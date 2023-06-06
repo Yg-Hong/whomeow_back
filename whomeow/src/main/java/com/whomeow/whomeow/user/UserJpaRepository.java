c671d5131066b37e6da963ad4c266ef762dc1574
Yg-Hong <lion0077v@gmail.com>
2023-06-06 17:38:42 +0900

    [config]AWS RDS 연결

--- whomeow/src/main/java/com/whomeow/whomeow/user/UserJpaRepository.java
+++ whomeow/src/main/java/com/whomeow/whomeow/user/UserJpaRepository.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, Long> {
-    @Query(value="SELECT * FROM USER U WHERE U.USEREMAIL = ?1", nativeQuery = true)
+    @Query(value="SELECT * FROM user U WHERE U.USEREMAIL = ?1", nativeQuery = true)
     User findByUserEmail(String userEmail);
 
-    @Query(value = "SELECT USEREMAIL FROM USER U WHERE U.USERNAME = ?1 and U.PHONENUMBER = ?2", nativeQuery = true)
+    @Query(value = "SELECT USEREMAIL FROM user U WHERE U.USERNAME = ?1 and U.PHONENUMBER = ?2", nativeQuery = true)
     String findEmailByNameAndPhoneNumber(String userName, String phoneNumber);
 }
